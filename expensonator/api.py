c47e670004ffeca07115608671241fe00be17f67
Matt Haigh <matthew.e.haigh@gmail.com>
2014-02-23 13:12:51 -0500

    Add table of expenses

--- expensonator/api.py
+++ expensonator/api.py
@@ -1,3 +1,4 @@
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 
 from expensonator.models import Expense
@@ -7,3 +8,6 @@ class ExpenseResource(ModelResource):
     class Meta:
         queryset = Expense.objects.all()
         excludes = ["created", "updated"]
+        # WARNING: Tastypie docs say that this is VERY INSECURE!
+        # For development only!
+        authorization = Authorization()
--- expensonator/models.py
+++ expensonator/models.py
@@ -4,7 +4,7 @@ from taggit.managers import TaggableManager
 class Expense(models.Model):
     name = models.CharField(max_length=200)
     merchant = models.CharField(max_length=200, blank=True)
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateField()
 
     tags = TaggableManager()
