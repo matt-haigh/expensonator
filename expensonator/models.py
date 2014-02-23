c4aaf6d6c6ac630a12e8ba01e680398c10f0a554
Matt Haigh <matthew.e.haigh@gmail.com>
2014-02-23 13:51:23 -0500

    Add budget categories

--- expensonator/models.py
+++ expensonator/models.py
@@ -7,6 +7,27 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateField()
 
+    BUDGET_CATEGORY_ENTERTAINMENT = u"Entertainment"
+    BUDGET_CATEGORY_FOOD = u"Food"
+    BUDGET_CATEGORY_NONE = u"None"
+    BUDGET_CATEGORY_TRANSPORTATION = u"Transportation"
+    BUDGET_CATEGORY_TRAVEL = u"Travel"
+    BUDGET_CATEGORY_HOUSEWARES = u"Housewares"
+    BUDGET_CATEGORY_UTILITIES = u"Utilities"
+    BUDGET_CATEGORY_CHOICES = (
+        (BUDGET_CATEGORY_ENTERTAINMENT, BUDGET_CATEGORY_ENTERTAINMENT),
+        (BUDGET_CATEGORY_FOOD, BUDGET_CATEGORY_FOOD),
+        (BUDGET_CATEGORY_NONE, BUDGET_CATEGORY_NONE),
+        (BUDGET_CATEGORY_TRANSPORTATION, BUDGET_CATEGORY_TRANSPORTATION),
+        (BUDGET_CATEGORY_TRAVEL, BUDGET_CATEGORY_TRAVEL),
+        (BUDGET_CATEGORY_HOUSEWARES, BUDGET_CATEGORY_HOUSEWARES),
+        (BUDGET_CATEGORY_UTILITIES, BUDGET_CATEGORY_UTILITIES))
+    BUDGET_CATEGORY_DEFAULT = BUDGET_CATEGORY_NONE
+    budget_category = models.CharField(
+        max_length=20,
+        choices=BUDGET_CATEGORY_CHOICES,
+        default=BUDGET_CATEGORY_DEFAULT)
+
     tags = TaggableManager()
 
     created = models.DateTimeField(auto_now_add=True)
